a96a1bf4b931c39e9f869f3b6154a7676a8b9487
vinaychaurasiya <contacctserver@gmail.com>
2020-10-17 20:46:27 +0530

    made date() reusable

---
+++ date.js
@@ -0,0 +1,22 @@
+module.exports.getDate = function (){
+
+const today = new Date();
+
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return today.toLocaleDateString("en-US", options);
+}
+
+module.exports.getDay = function (){
+
+    const today = new Date();
+    
+        const options = {
+            weekday: 'long'
+        }
+        return today.toLocaleDateString("en-US", options);
+    }
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const app = express();
 
 let items = ["Yoga 5am - 6am", "Meditation 6:10am - 6:30 am", "Study 8pm 11pm"];
@@ -10,24 +11,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
 
     app.get("/", function (req, res) {
-        var today = new Date();
-
-        var options = {
-            weekDay: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        }
-
-        var day = today.toLocaleDateString("en-US", options);
+        const day = date.getDate();
 
         res.render("list", { listTitle: day, newListItems: items });
     });
 
     app.post("/", function (req, res) {
         let item = req.body.newItem;
-        console.log(req.body)
-        console.log(item)
 
         if(req.body.list === "Work List"){
             workItems.push(item);
